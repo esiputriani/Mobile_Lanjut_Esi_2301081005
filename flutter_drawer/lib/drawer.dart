9912a89f0f42a6d70297520b94d08da4a3f8c0ba
esiputriani <167851090+esiputriani@users.noreply.github.com>
2024-09-25 07:43:15 +0700

    flutter_drawer

---
+++ flutter_drawer/lib/drawer.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter_drawer/home_page.dart';
+import 'package:flutter_drawer/setting_page.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: 250,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 160,
+            color: Colors.green,
+            alignment: Alignment.bottomLeft,
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Menu",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          SizedBox(),
+          ListTile(
+            onTap: () {
+              Navigator.of(context).pushNamed(MyHome.routesName);
+            },
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.of(context).pushNamed(SettingPage.routesName);
+            },
+            leading: Icon(Icons.settings),
+            title: Text("Setting"),
+          ),
+        ],
+      ),
+    );
+  }
+}
