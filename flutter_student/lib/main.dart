5f5d1e57277c20bac6342a63ae0944cb5f8f1de8
esiputriani <167851090+esiputriani@users.noreply.github.com>
2024-11-06 11:33:25 +0700

    flutter student

---
+++ flutter_student/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './pages/home_page.dart';
+import 'pages/add_student_page.dart';
+import 'pages/detail_student_page.dart';
+import 'providers/students.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Students(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
+        routes: {
+          AddStudent.routeName: (context) => AddStudent(),
+          DetailStudent.routeName: (context) => DetailStudent(),
+        },
+      ),
+    );
+  }
+}
